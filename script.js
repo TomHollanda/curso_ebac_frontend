932fa725c1924320be5f108586629958b3ea1b3a
TomHollanda <eltonhollanda@gmail.com>
2024-01-23 16:49:57 -0300

    corrige estilo no footer

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 // script.js
 
+// btnVoltarAoTopo
+
 document.addEventListener('DOMContentLoaded', function() {
     var btnVoltarAoTopo = document.getElementById('btnVoltarAoTopo');
 
     btnVoltarAoTopo.addEventListener('click', function() {
-        // Rolagem suave para o topo da página
+        // controle: rolagem botão
         window.scrollTo({
             top: 0,
             behavior: 'smooth'
         });
     });
 
-    // Exibe ou oculta o botão com base na posição de rolagem
+    // controle: exibição botão
     window.addEventListener('scroll', function() {
         if (window.scrollY > 100) {
             btnVoltarAoTopo.style.display = 'block';
@@ -20,3 +22,24 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+//muda a cor do btnVoltarAoTopo if footer ( ou mudar background no css)
+
+$(document).ready(function () {
+    var btnVoltarAoTopo = $('#btnVoltarAoTopo');
+    var footer = $('#footer');
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() + $(window).height() > footer.offset().top) {
+            btnVoltarAoTopo.css('background-color', '#000000');
+        } else {
+            btnVoltarAoTopo.css('background-color', '#000000');
+        }
+    });
+
+    btnVoltarAoTopo.click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 500);
+    });
+});
+
+
