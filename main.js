30ec1dd0690362e35c2d26e7e61819ab7f37f27d
TomHollanda <eltonhollanda@gmail.com>
2024-04-09 16:15:40 -0300

    Adiciona botão deletar imagem

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     $('header button').click(function(){
         $('form').slideDown();
-    })
+    });
 
     $('#botao-cancelar').click(function() {
         $('form').slideUp();
-    })
+    });
 
     $('form').on('submit',function(e){
         e.preventDefault();
@@ -13,17 +13,23 @@ $(document).ready(function(){
         const novoItem = $('<li style="display: none"></li>');
         $(`<img src="${enderecoDaNovaImagem}"/>`).appendTo(novoItem);
         $(`
-        
             <div class="overlay-imagem-link">
                 <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
                     Ver imagem em tamanho real
-                </a
-            <div/>
-
+                </a>
+                <button class="botao-deletar">Deletar</button>
+            </div>
         `).appendTo(novoItem);
         $(novoItem).appendTo('ul');
         $(novoItem).fadeIn(600);
         $('#endereco-imagem-nova').val('');
-    })
-})
 
+        $('.botao-deletar').click(function(){
+            $(this).closest('li').remove();
+        });
+    });
+
+    $('ul').on('click', '.botao-deletar', function(){
+        $(this).closest('li').remove();
+    });
+});
