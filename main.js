8c3d526a32cf24b9730e8709c2b239bd0fc7a9ad
TomHollanda <eltonhollanda@gmail.com>
2024-04-25 20:02:11 -0300

    Adiciona validação de formulários utilizando jQuery Validate

--- main.js
+++ main.js
@@ -5,5 +5,43 @@ $(document).ready(function(){
     $('.menu-hbg').click(function(){
         $('nav').slideToggle();
     })
+    $('#fone').mask ('(00) 00000-0000', {
+        placeholder: '(99) 9 9999-9999'
+    })
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            fone: {
+                required: true
+            },
+            mensagem: {
+                required:true,
+            },
+            veiculoDeInteresse: {
+                required: false,
+            }
+        },
+        messages: {
+            nome: 'Por favor, insira o seu nome',
+            email: 'Por favor, insira um email válido',
+            fone: 'Por favor, insira um número de telefone válido',
+        },
+        submitHandler: function(form){
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if(camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+    })
+
 });
 
